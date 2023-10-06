d6d085ecba6ae98959b57719c5abfccd35d6bd5c
odanroterif <143523077+odanroterif@users.noreply.github.com>
2023-10-06 11:32:41 -0300

    Update tarefadecasa.c

--- vscodi/tarefadecasa.c
+++ vscodi/tarefadecasa.c
@@ -14,44 +14,65 @@ int main()
 //----------------------------
     printf("digite o valor do produto: ");
     scanf("%f", &objetcprice);
-    printf("1 para pagamento á vista e 2 para parcelado");
+    printf("\n\n1 para pagamento á vista e 2 para parcelado");
     printf("\ndigite a forma de pagamento: ");
     scanf("%i", &option);
     fflush(stdin);
+    system("cls || flush");
     switch (option)
     {
     case 1:
         desconto = (objetcprice)*10 / 100;
-        printf("valor a pagar: %.2f", objetcprice - desconto);
+        printf("valor do produto: %.2f", objetcprice);
+        printf("\nopção de pagamento: á vista");
+        printf("\nvalor do desconto: %.2f", desconto);
+        printf("\nvalor a pagar: %.2f", objetcprice - desconto);
         break;
     case 2:
         printf("quantas parcelas deseja?: ");
         scanf("%i", &parcelas);
-    default:
-        printf("não temos esta opção");
-        break;
+    } if ((option != 1) && (option != 2)) {
+        printf("\nnão temos esta opção");
     }
     if (option == 2)
     {
         switch (parcelas)
         {
         case 1:
-            printf("valor das parcelas: %.2f", objetcprice / 1);
+            printf("\nvalor do produto: %.2f", objetcprice);
+            printf("\nopção de pagamento: parcelado");
+            printf("\nnúmero de parcelas: %i", parcelas);
+            printf("\nvalor das parcelas: %.2f", objetcprice / 1);
             break;
         case 2:
-            printf("valor das parcelas: %.2f", objetcprice / 2);
+            printf("\nvalor do produto: %.2f", objetcprice);
+            printf("\nopção de pagamento: parcelado");
+            printf("\nnúmero de parcelas: %i", parcelas);
+            printf("\nvalor das parcelas: %.2f", objetcprice / 2);
             break;
         case 3:
-            printf("valor das parcelas: %.2f", objetcprice / 3);
+            printf("\nvalor do produto: %.2f", objetcprice);
+            printf("\nopção de pagamento: parcelado");
+            printf("\nnúmero de parcelas: %i", parcelas);
+            printf("\nvalor das parcelas: %.2f", objetcprice / 3);
             break;
         case 4:
-            printf("valor das parcelas: %.2f", objetcprice / 4);
+            printf("\nvalor do produto: %.2f", objetcprice);
+            printf("\nopção de pagamento: parcelado");
+            printf("\nnúmero de parcelas: %i", parcelas);
+            printf("\nvalor das parcelas: %.2f", objetcprice / 4);
             break;
         case 5:
-            printf("valor das parcelas: %.2f", objetcprice / 5);
+            printf("\nvalor do produto: %.2f", objetcprice);
+            printf("\nopção de pagamento: parcelado");
+            printf("\nnúmero de parcelas: %i", parcelas);
+            printf("\nvalor das parcelas: %.2f", objetcprice / 5);
             break;
         case 6:
-            printf("valor das parcelas: %.2f", objetcprice / 6);
+            printf("\nvalor do produto: %.2f", objetcprice);
+            printf("\nopção de pagamento: parcelado");
+            printf("\nnúmero de parcelas: %i", parcelas);
+            printf("\nvalor das parcelas: %.2f", objetcprice / 6);
             break;
 
         default:
