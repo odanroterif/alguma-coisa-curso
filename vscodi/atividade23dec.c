5460da10fa62d93eb204ab2cc0400e41ef0e483a
odanroterif <143523077+odanroterif@users.noreply.github.com>
2023-10-16 15:33:03 -0300

    Add files via upload

---
+++ vscodi/atividade23dec.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include <ctype.h>
+//-----------------------------
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+//variable declaration----------
+    int number;
+    int result = 0;
+    char answer[999];
+    int count = 0;
+//------------------------------
+    do
+    {
+        printf("digite um número: ");
+        scanf("%i", &number);
+        if (number >= 0)
+        {
+            result += number;
+            count++;
+        }
+        fflush(stdin);
+        printf("deseja inserir mais um?: ");
+        gets(answer);
+        system("cls || clear");
+    } while (strcmp(answer, "sim") == 0 || strcmp(answer, "SIM") == 0 || strcmp(answer, "Sim") == 0);
+    printf("\no total dos números digitados é %i,esta é a média: %i", result, result / count);
+    return 0;
+}
