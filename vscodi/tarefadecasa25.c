35066af2d83154873ee194cad5ea1bb75641e052
odanroterif <143523077+odanroterif@users.noreply.github.com>
2023-10-17 14:20:04 -0300

    Add files via upload

---
+++ vscodi/tarefadecasa25.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include <ctype.h>
+//-----------------------------
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+//variable declaration----------
+    float money;
+    int age;
+    char gender;
+    int minage;
+    int maxage;
+    int count = 0;
+    int endquest;
+    float teammoney = 0;
+    int generalcount = 0;
+//------------------------------
+    do
+    {
+        printf("código | números para iniciar pesquisa ou adicinar mais pessoas a pesquisa");
+        printf("\n1      | adicionar/começar");
+        printf("\n2      | encerrar/cancelar");
+        printf("\ncomeçar pesquisa/inserir mais pessoas?: ");
+        scanf("%i", &endquest);
+        if (endquest != 1 && endquest != 2)
+        {
+            printf("não tem essa opção digite novamente: ");
+            scanf("%i", &endquest);
+        }
+        system("cls || clear");
+        switch (endquest)
+        {
+
+        case 1:
+            do
+            {
+                fflush(stdin);
+                printf("F para fêminino e M para masculino");
+                printf("\ndigite seu sexo: ");
+                scanf("%c", &gender);
+                gender = toupper(gender);
+                if (gender != 'F' && gender != 'M')
+                {
+                    printf("afirmação invalida, digite novamente: ");
+                    scanf("%c", &gender);
+                }
+                system("cls || clear");
+            } while (gender != 'F' && gender != 'M');
+            do
+            {
+                fflush(stdin);
+                printf("digite sua idade: ");
+                scanf("%i", &age);
+                if (age < 10)
+                {
+                    printf("essa idade esta fora da faixa de pesquisa,digite outra: ");
+                    scanf("%i", &age);
+                }
+
+  if (age < age)
+            {
+                minage = age;
+            }
+             if (age > age)
+            {
+                maxage = age;
+            }
+            } while (age < 10);
+           
+            printf("qual o valor do seu sálario?: ");
+            scanf("%f", &money);
+            teammoney += money;
+            generalcount++;
+            if (gender == 'F' && money > 5000)
+            {
+                count++;
+            }
+        case 2:
+            break;
+        }
+    } while (endquest != 2);
+    system("cls || clear");
+    printf("média salarial: %.2f", teammoney / generalcount);
+    printf("\nmaior idade dos pesquisados: %i", minage);
+    printf("\nmenor idade dos pesquisados: %i", maxage);
+    printf("\nquantidade de mulheres com sálario acima de R$5.000,00: %i", count);
+    return 0;
+}
