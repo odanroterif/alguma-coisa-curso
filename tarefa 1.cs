708c50fccea6ce5bbf7593cb793e3ec025ca69b0
odanroterif <143523077+odanroterif@users.noreply.github.com>
2023-11-25 17:17:07 -0300

    Add files via upload

---
+++ tarefa 1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//------------------------------------------
+namespace tarefa_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minstock;
+            int maxstock;
+            float stock;
+
+            Console.Write("digite o estoque minimo: ");
+            minstock = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.Write("digite o estoque máximo: ");
+            maxstock = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            stock = (maxstock + minstock) / 2f;
+            Console.WriteLine("o estoque médio atual é de " + stock);
+            Console.ReadLine();
+        }
+    }
+}
